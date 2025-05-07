8e2bbbf147edff78ec77bccdc17d2456400897c3
1codequeen <queenyona21@gmail.com>
2025-05-07 15:26:50 +0200

    added local timezone on select

--- javascript/script.js
+++ javascript/script.js
@@ -26,6 +26,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "Africa/Johannesburg") {
+    let cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -41,7 +44,6 @@ function updateCity(event) {
   </div>
   `;
 }
-
 updateTime();
 setInterval(updateTime, 1000);
 
